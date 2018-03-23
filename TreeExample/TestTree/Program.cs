53dcad5ea937bb634fea0692debb4057573473b8
jmoll <jmoll@LAPTOP-KDV1IDLA>
2018-03-22 22:43:57 -0600

    Creation of a New Branch Create NodeB

--- TreeExample/TestTree/Program.cs
+++ TreeExample/TestTree/Program.cs
@@ -14,60 +14,87 @@ namespace TestTree
     {
         static void Main(string[] args)
         {
-            var estudiantes = new List<Estudiante>
-            {
-                new Estudiante
-                {
-                    Id = 1205917,
-                    Name = "Rodrigo",
-                    LastName = "Aguilar",
-                    Facultity = "Ingeniería",
-                    Career = "Ingeniería en Inormática y Sistemas"
-                },
+            #region first Commit
+            //var estudiantes = new List<Estudiante>
+            //{
+            //    new Estudiante
+            //    {
+            //        Id = 1205917,
+            //        Name = "Rodrigo",
+            //        LastName = "Aguilar",
+            //        Facultity = "Ingeniería",
+            //        Career = "Ingeniería en Inormática y Sistemas"
+            //    },
 
-                new Estudiante
-                {
-                    Id = 1109117,
-                    Name = "Emmanuel",
-                    LastName = "Alvarado",
-                    Facultity = "Ingeniería",
-                    Career = "Ingeniería en Inormática y Sistemas"
-                },
-            };
+            //    new Estudiante
+            //    {
+            //        Id = 1109117,
+            //        Name = "Emmanuel",
+            //        LastName = "Alvarado",
+            //        Facultity = "Ingeniería",
+            //        Career = "Ingeniería en Inormática y Sistemas"
+            //    },
+            //};
 
-            //Internamente hace Open y Close
-            using (var fs = new FileStream("C:\\Users\\llaaj\\Desktop\\test.txt", FileMode.OpenOrCreate))
+            ////Internamente hace Open y Close
+            //using (var fs = new FileStream("C:\\Users\\llaaj\\Desktop\\test.txt", FileMode.OpenOrCreate))
+            //{
+            //    foreach (var item in estudiantes)
+            //    {
+            //        fs.Write(ByteGenerator.ConvertToBytes(item.ToFixedSizeString()), 0, item.FixedSizeText);
+            //        //fs.Write(ByteGenerator.ConvertToBytes())
+            //    }
+            //}
+
+            ////Leer
+            //var raiz = 2;
+            //var buffer = new byte[Estudiante.FixedSize];
+            //using (var fs = new FileStream("C:\\Users\\llaaj\\Desktop\\test.txt", FileMode.OpenOrCreate))
+            //{
+            //    fs.Seek((raiz -1) * Estudiante.FixedSize, SeekOrigin.Begin);
+            //    fs.Read(buffer, 0, Estudiante.FixedSize);
+            //}
+
+            ////Mostrar
+            //var nodeString = ByteGenerator.ConvertToString(buffer);
+            //var values = nodeString.Split('|');
+            //var student = new Estudiante
+            //{
+            //    Id = Convert.ToInt32(values[0]),
+            //    Name = values[1].Trim(),
+            //    LastName = values[2].Trim(),
+            //    Facultity = values[3].Trim(),
+            //    Career = values[4].Trim()
+            //};
+            //Console.Write(student.ToString());
+
+            //Console.ReadKey();
+            #endregion
+
+            NodeB<int> node = new NodeB<int>(5);
+            Random rand = new Random();
+            while (!node.OverFlowNodeValues)
             {
-                foreach (var item in estudiantes)
-                {
-                    fs.Write(ByteGenerator.ConvertToBytes(item.ToFixedSizeString()), 0, item.FixedSizeText);
-                    //fs.Write(ByteGenerator.ConvertToBytes())
-                }
+                node.AddValueToList(rand.Next(1, 20));
             }
-
-            //Leer
-            var raiz = 2;
-            var buffer = new byte[Estudiante.FixedSize];
-            using (var fs = new FileStream("C:\\Users\\llaaj\\Desktop\\test.txt", FileMode.OpenOrCreate))
+            NodeB<int> A;
+            NodeB<int> B;
+            int m;
+            node.SplitNode(out A, out B, out m);
+            string text = string.Empty;
+            foreach(int num in A.Values)
             {
-                fs.Seek((raiz -1) * Estudiante.FixedSize, SeekOrigin.Begin);
-                fs.Read(buffer, 0, Estudiante.FixedSize);
+                text += num.ToString() + ",";
             }
-
-            //Mostrar
-            var nodeString = ByteGenerator.ConvertToString(buffer);
-            var values = nodeString.Split('|');
-            var student = new Estudiante
+            Console.WriteLine(text);
+            Console.WriteLine(m.ToString());
+            text = string.Empty;
+            foreach (int num in B.Values)
             {
-                Id = Convert.ToInt32(values[0]),
-                Name = values[1].Trim(),
-                LastName = values[2].Trim(),
-                Facultity = values[3].Trim(),
-                Career = values[4].Trim()
-            };
-            Console.Write(student.ToString());
-
-            Console.ReadKey();
+                text += num.ToString() + ",";
+            }
+            Console.WriteLine(text);
+            Console.Read();
         }
     }
 }
--- TreeExample/TreeExample/BTree.cs
+++ TreeExample/TreeExample/BTree.cs
@@ -8,30 +8,159 @@ using TreeExample.Interface;
 
 namespace TreeExample
 {
-    public class Node<T> where T: IComparable
+    #region firstCommit
+    //public class Node<T> where T: IComparable
+    //{
+    //    //Listado de elementos (nodos)
+    //    //Listado de hijos (nodos)
+    //    public List<T> Values { get; set; }
+    //    public List<int> Children { get; set; }
+    //    public int Parent { get; set; }
+    //    public int Position { get; set; }
+    //}
+
+    //public class BTree<T> where T: IComparable, IFixedSizeText
+    //{
+    //    //Se guarda en disco
+    //    //Tiene raíz
+    //    //Tiene un orden
+    //    //Siempre está balanceado
+    //    //No repetir valores
+
+    //    public int Orden { get; set; }
+
+    //    //public Node<T> Raiz { get; set; }
+    //    //para no crearlo en memoria y solo obtener la posición en la que esté en el archivo
+    //    public int Root { get; set; }
+
+    //    public FileStream MyProperty { get; set; }
+    //}
+    #endregion
+
+    /// <summary>
+    /// B-Tree Node
+    /// </summary>
+    /// <typeparam name="T">Object type in B-Tree</typeparam>
+    public class NodeB<T> where T: IComparable
     {
-        //Listado de elementos (nodos)
-        //Listado de hijos (nodos)
-        public List<T> Values { get; set; }
-        public List<int> Children { get; set; }
-        public int Parent { get; set; }
-        public int Position { get; set; }
+        /// <summary>
+        /// Node Values
+        /// </summary>
+        public List<T> Values { get; private set; }
+        /// <summary>
+        /// Node Children
+        /// </summary>
+        public List<NodeB<T>> Children { get; set; }
+        /// <summary>
+        /// Order of Owner Tree
+        /// </summary>
+        public int Order { get; private set; }
+
+        /// <summary>
+        /// Node Builder
+        /// </summary>
+        /// <param name="order">Order of Owner Tree</param>
+        public NodeB(int order)
+        {
+            Order = order;
+            Values = new List<T>();
+            Children = new List<NodeB<T>>();
+        }
+
+        /// <summary>
+        /// Node Values Invalid Count
+        /// </summary>
+        public bool OverFlowNodeValues
+        {
+            get
+            {
+                return Values.Count > Order - 1;
+            }
+        }
+
+        /// <summary>
+        /// Node Children Invalid Count
+        /// </summary>
+        public bool OverFlowNodeChildren
+        {
+            get
+            {
+                return Children.Count > Order;
+            }
+        }
+
+        /// <summary>
+        /// Node Value Invalid Count
+        /// </summary>
+        public bool UnderFlowNodeValues
+        {
+            get
+            {
+                return Values.Count < Order / 2 - 1;
+            }
+        }
+
+        /// <summary>
+        /// Node Children Invalid Count
+        /// </summary>
+        public bool UnderFlowNodeChildren
+        {
+            get
+            {
+                return Children.Count < Order / 2;
+            }
+        }
+
+        /// <summary>
+        /// Add Value to List Values
+        /// </summary>
+        /// <param name="value">Value to Add</param>
+        public void AddValueToList(T value)
+        {
+            Values.Add(value);
+            Values.Sort();
+        }
+
+        /// <summary>
+        /// Divides Nodo into 2 parts by the middle value
+        /// </summary>
+        /// <param name="nodeLeft">Left partition</param>
+        /// <param name="nodeRight">Right partition</param>
+        /// <param name="valueToRoot">Middle value</param>
+        public void SplitNode(out NodeB<T> nodeLeft, out NodeB<T> nodeRight, out T valueToRoot)
+        {
+            nodeLeft = new NodeB<T>(Order);
+            nodeRight = new NodeB<T>(Order);
+            int i = 0;
+            while (i < Values.Count / 2)
+            {
+                nodeLeft.Values.Add(Values[i]);
+                nodeLeft.addChildren(Children, i);
+                i++;
+            }
+            valueToRoot = Values[i];
+            i++;
+            while (i < Values.Count)
+            {
+                nodeRight.Values.Add(Values[i]);
+                nodeRight.addChildren(Children, i + 1);
+                i++;
+            }
+        }
+
+        /// <summary>
+        /// Add children nodes from another node
+        /// </summary>
+        /// <param name="PrevchildrenList">List of children from other node</param>
+        /// <param name="position">Position in List</param>
+        private void addChildren(List<NodeB<T>> PrevchildrenList, int position)
+        {
+            try
+            {
+                Children.Add(PrevchildrenList[position]);
+            }
+            catch { }
+        }
     }
 
-    public class BTree<T> where T: IComparable, IFixedSizeText
-    {
-        //Se guarda en disco
-        //Tiene raíz
-        //Tiene un orden
-        //Siempre está balanceado
-        //No repetir valores
-
-        public int Orden { get; set; }
-
-        //public Node<T> Raiz { get; set; }
-        //para no crearlo en memoria y solo obtener la posición en la que esté en el archivo
-        public int Root { get; set; }
-
-        public FileStream MyProperty { get; set; }
-    }
 }
